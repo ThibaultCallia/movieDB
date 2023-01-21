414776826d01a76bd97f4d4e549c03ccdd0861dd
Thibault Calliauw <calliauw.t@gmail.com>
2023-01-21 10:02:02 +0100

    setting up front

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Movie DB</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/new">New movie</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
